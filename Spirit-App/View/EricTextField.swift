7e643ff1d18974ede478b45f9afa93366e39f5ca
Eric Walters <ericwalters@erics-mbp.attlocal.net>
2017-12-29 18:41:09 -0500

    Added Title to register

---
+++ Spirit-App/View/EricTextField.swift
@@ -0,0 +1,9 @@
+//
+//  EricTextField.swift
+//  Spirit-App
+//
+//  Created by Eric Walters on 12/27/17.
+//  Copyright © 2017 Eric Walters. All rights reserved.
+//
+
+import Foundation
---
+++ Spirit-App/View/UIExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIExtension.swift
+//  Spirit-App
+//
+//  Created by Eric Walters on 12/28/17.
+//  Copyright © 2017 Eric Walters. All rights reserved.
+//
+
+import Foundation
